74149dab68ab11de12508700b149e4a15516bfd3
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2022-06-26 01:10:15 +0300

    Icons for all buttons

--- svg2ico.py
+++ svg2ico.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import QBuffer, QIODevice, QByteArray
 from PyQt5.QtGui import QImage, QIcon
 from PyQt5.QtWidgets import QApplication
 
-path = "res/buttons/resume.svg"
+path = "res/buttons/drag_brick.svg"
 
 app = QApplication([])
 src = QIcon(path)
